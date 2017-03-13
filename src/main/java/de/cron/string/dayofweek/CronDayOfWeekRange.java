84a7656dcb1bb64fd3745d304c3da593742c342e
Stefan Rademacher <s.rademacher@live.de>
2017-03-13 21:36:41 +0100

    fix range string representations

--- src/main/java/de/cron/string/dayofweek/CronDayOfWeekRange.java
+++ src/main/java/de/cron/string/dayofweek/CronDayOfWeekRange.java
@@ -2,19 +2,22 @@ package de.cron.string.dayofweek;
 
 import java.time.DayOfWeek;
 
+import com.google.common.base.Preconditions;
+
 public class CronDayOfWeekRange implements CronDayOfWeek {
 
 	private DayOfWeek fromDayOfWeek;
 	private DayOfWeek untilDayOfWeek;
 
 	public CronDayOfWeekRange(DayOfWeek fromDayOfWeek, DayOfWeek untilDayOfWeek) {
+		Preconditions.checkArgument(!fromDayOfWeek.equals(untilDayOfWeek));
 		this.fromDayOfWeek = fromDayOfWeek;
 		this.untilDayOfWeek = untilDayOfWeek;
 	}
 
 	@Override
 	public String toString() {
-		return fromDayOfWeek + "-" + untilDayOfWeek;
+		return fromDayOfWeek.getValue() + "-" + untilDayOfWeek.getValue();
 	}
 
 }
--- src/main/java/de/cron/string/month/CronMonthRange.java
+++ src/main/java/de/cron/string/month/CronMonthRange.java
@@ -2,19 +2,22 @@ package de.cron.string.month;
 
 import java.time.Month;
 
+import com.google.common.base.Preconditions;
+
 public class CronMonthRange implements CronMonth {
 
 	private Month fromMonth;
 	private Month untilMonth;
 
 	public CronMonthRange(Month fromMonth, Month untilMonth) {
+		Preconditions.checkArgument(!fromMonth.equals(untilMonth));
 		this.fromMonth = fromMonth;
 		this.untilMonth = untilMonth;
 	}
 
 	@Override
 	public String toString() {
-		return fromMonth + "-" + untilMonth;
+		return fromMonth.getValue() + "-" + untilMonth.getValue();
 	}
 
 }
