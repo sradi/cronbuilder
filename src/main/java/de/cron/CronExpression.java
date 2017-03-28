fba1fde20733c553fa2024a2ba1b0863f963e5b1
Stefan Rademacher <s.rademacher@live.de>
2017-03-28 16:32:59 +0200

    CronExpression interface -> class

--- src/main/java/de/cron/CronExpression.java
+++ src/main/java/de/cron/CronExpression.java
@@ -1,9 +1,44 @@
 package de.cron;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.cron.elements.CronElement;
 
-public interface CronExpression {
+public class CronExpression {
 	
-	CronExpression prepend(CronElement part);
+	private static final String CRON_ELEMENT_SEPARATOR = " ";
+	
+	private List<CronElement> parts = new ArrayList<>();
+	
+	private CronExpression() {
+	}
+	
+	public CronExpression(CronElement firstPart) {
+		this.parts.add(firstPart);
+	}
+	
+	/**
+	 * Always return an immutable copy of the current object
+	 */
+	public CronExpression prepend(CronElement newPart) {
+		CronExpression complexCron = new CronExpression();
+		for (CronElement p : parts) {
+			complexCron.parts.add(p);
+		}
+		complexCron.parts.add(0, newPart);
+		return complexCron;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder cronStringBuilder = new StringBuilder();
+		parts.forEach(p ->
+			cronStringBuilder
+				.append(p)
+				.append(CRON_ELEMENT_SEPARATOR)
+		);
+		return cronStringBuilder.substring(0, cronStringBuilder.length()-1);
+	}
 
 }
